e6e85184d6bf6a4287da466e9ed220c1bc244acf
wt-personal <waltteri.tulijoki@gmail.com>
2025-12-07 19:38:09 +0200

    Update convert-HTML.py

--- convert-HTML.py
+++ convert-HTML.py
@@ -1,4 +1,4 @@
-input_file = input("tiedostonimi: ")  # your HTML file
+input_file = input("filename: ")  # your HTML file
 output_file = input("output: ")       # output file with client.println() lines
 
 with open(input_file, "r", encoding="utf-8") as f:
